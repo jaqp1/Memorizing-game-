f5ae71f7a4cb6ef636d8ea131bb25cdcc87cb324
jaqp1 <kuba.wenek@wp.pl>
2025-05-21 19:48:57 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const shuffleButton = document.getElementById("shuffleButton");
+
+
+shuffleButton.addEventListener("click", () => {
+    shuffleImages();
+});
+
+function shuffleImages(){
+    const images = document.querySelectorAll(".card ");
+
+    const positions = [];
+
+    for (let row = 1; row <= 4; row++) {
+        for (let col = 1; col <= 4; col++) {
+            positions.push({ row, col });
+        }
+    }
+
+    for (let i = positions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [positions[i], positions[j]] = [positions[j], positions[i]];
+    }
+    images.forEach((image, index) => {
+        const pos = positions[index];
+        image.style.gridRow = pos.row;
+        image.style.gridColumn = pos.col;
+    });
+}
